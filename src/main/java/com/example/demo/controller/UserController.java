da4065479ba0d0919440b84226f5c42892cb4dbe
Kushal <kushalburad07@gmail.com>
2025-01-14 12:30:29 +0530

    change file structure

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,9 @@
-package com.example.demo;
+package com.example.demo.controller;
 
+import com.example.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
